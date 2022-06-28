f70f2592a0db92216ec82819584d1f330a302e7d
zfuqiong <zfuqiong@ifreegroup.com>
2022-06-28 23:14:42 +0800

    update

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,18 @@
   windows_subsystem = "windows"
 )]
 
+use tauri::{CustomMenuItem, Submenu, Menu, MenuItem, generate_context};
+
 fn main() {
-  let context = tauri::generate_context!();
+  let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+  let close = CustomMenuItem::new("close".to_string(), "Close");
+  let submenu = Submenu::new("File", Menu::new().add_item(quit).add_item(close));
+  let menu = Menu::new()
+    .add_native_item(MenuItem::Copy)
+    .add_item(CustomMenuItem::new("hide", "Hide"))
+    .add_submenu(submenu);
+
+  let context = generate_context!();
   tauri::Builder::default()
     .menu(tauri::Menu::os_default(&context.package_info().name))
     .run(context)
