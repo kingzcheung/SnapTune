b95067327af91f403a75e6103b91d45916117d5e
kingzcheung <kingzcheung@gmail.com>
2022-06-29 18:36:23 +0800

    更新测试

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,8 +1,7 @@
-use std::{fmt::{Display}, path::Path, };
+use std::{fmt::Display, path::Path};
 
 use image::ImageFormat;
 use serde::{Deserialize, Serialize};
-
 
 #[derive(Serialize, Deserialize, Debug)]
 pub enum Format {
@@ -18,31 +17,30 @@ pub enum Format {
     DDS,
     TGA,
     OpenEXR,
-    Farbfeld
+    Farbfeld,
 }
 
 impl Display for Format {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Format::PNG => write!(f,"png"),
-            Format::JPEG => write!(f,"jpg"),
-            Format::GIF => write!(f,"gif"),
-            Format::BMP => write!(f,"bmp"),
-            Format::ICO => write!(f,"ico"),
-            Format::TIFF => write!(f,"tiff"),
-            Format::WebP => write!(f,"webp"),
-            Format::AVIF => write!(f,"avif"),
-            Format::PNM => write!(f,"pnm"),
-            Format::TGA => write!(f,"gta"),
-            Format::OpenEXR => write!(f,"exr"),
-            Format::Farbfeld => write!(f,"ff"),
-            Format::DDS => write!(f,"dds"),
+            Format::PNG => write!(f, "png"),
+            Format::JPEG => write!(f, "jpg"),
+            Format::GIF => write!(f, "gif"),
+            Format::BMP => write!(f, "bmp"),
+            Format::ICO => write!(f, "ico"),
+            Format::TIFF => write!(f, "tiff"),
+            Format::WebP => write!(f, "webp"),
+            Format::AVIF => write!(f, "avif"),
+            Format::PNM => write!(f, "pnm"),
+            Format::TGA => write!(f, "gta"),
+            Format::OpenEXR => write!(f, "exr"),
+            Format::Farbfeld => write!(f, "ff"),
+            Format::DDS => write!(f, "dds"),
         }
     }
 }
 
-
-pub async fn image2x(x: Format,source: String)-> Result<String,anyhow::Error>{
+pub async fn image2x(x: Format, source: String) -> Result<String, anyhow::Error> {
     let path = Path::new(source.as_str());
     let path = path.with_extension(x.to_string().as_str());
 
@@ -50,7 +48,23 @@ pub async fn image2x(x: Format,source: String)-> Result<String,anyhow::Error>{
 
     let err = anyhow::format_err!("格式错误");
     let image_format = ImageFormat::from_extension(x.to_string().as_str()).ok_or(err);
-    
+
     img.save_with_format(path, image_format?)?;
     Ok(source)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    
+    #[tokio::test]
+    async fn test_image2x(){
+        
+        let source = "testdata/test.jpg"; 
+        let result_path = std::path::Path::new(source).with_extension("png");
+        let res = image2x(Format::PNG, source.into()).await;
+        
+        assert!(res.is_ok());
+        assert!(std::fs::metadata(result_path).is_ok())
+    }
 }
