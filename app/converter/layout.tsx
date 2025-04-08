3ee35cf9dbf29bc688f3a158746a35deba35d45f
kingzcheung <kingzcheung@gmail.com>
2025-04-08 22:33:00 +0800

    refactor(layout): 重构布局组件并添加设置页面功能
    - 修改 ConverterLayout 和 SettingsLayout 组件的名称和样式
    - 在 settings 页面中添加压缩图片质量的设置功能
    - 调整设置页面的样式，增加标题和输入框

--- app/converter/layout.tsx
+++ app/converter/layout.tsx
@@ -1,4 +1,4 @@
-export default function AboutLayout({
+export default function ConverterLayout({
   children,
 }: {
   children: React.ReactNode;
--- app/settings/layout.tsx
+++ app/settings/layout.tsx
@@ -1,13 +1,11 @@
-export default function DocsLayout({
+export default function SettingsLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="inline-block max-w-lg text-center justify-center">
-        {children}
-      </div>
+    <section className="flex flex-col items-center justify-center w-full h-full py-2 px-4">
+      {children}
     </section>
   );
 }
