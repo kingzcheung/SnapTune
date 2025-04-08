1e7bfe4ab51392616d4b4b310c6bfad3ec68b161
kingzcheung <kingzcheung@gmail.com>
2025-04-08 22:33:00 +0800

    refactor(layout): 重构布局组件并添加设置页面功能
    - 修改 ConverterLayout 和 SettingsLayout 组件的名称和样式
    - 在 settings 页面中添加压缩图片质量的设置功能
    - 调整设置页面的样式，增加标题和输入框

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,9 +1,21 @@
-import { title } from "@/components/primitives";
+"use client";
+
+import { NumberInput, Slider } from "@heroui/react";
 
 export default function DocsPage() {
   return (
-    <div>
-      <h1 className={title()}>Docs</h1>
+    <div className="h-full w-full mt-2 p-4 bg-zinc-300/20 rounded-xl">
+      <h2 className="text-2xl">settings</h2>
+      <div className="mb-4 flex items-center justify-between">
+        <div className="text-zinc-600/70 dark:text-zinc-300/70">压缩图片质量</div>
+        <NumberInput
+          className="max-w-xs"
+          label="Quality"
+          step={5}
+          max={100}
+          min={70}
+        />
+      </div>
     </div>
   );
 }
